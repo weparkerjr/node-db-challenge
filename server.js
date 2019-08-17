68d32b7b063af795de4cf5fdaf7d452686beaa66
william-parker <edwinious@gmail.com>
2019-08-17 12:41:08 -0400

    Added knexfile

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
+// const ProjectRouter = require("./project-router");
 const server = express();
 
 server.use(express.json());
-
+// server.use("/api/project", ProjectRouter);
 
 server.get("/", (req, res) => {
   res.send("Database Homepage");
